e0e6660edc0e04d4ba33f211f2f8969dd5e26556
Rico Sta. Cruz <hi@ricostacruz.com>
2015-01-21 17:07:01 +0800

    Add simple tests

---
+++ index.js
@@ -0,0 +1,9 @@
+var TB = {};
+
+TB.Scope = function Scope() {};
+
+TB.event = function () {
+  return new Scope();
+};
+
+module.exports = TB;
---
+++ test/test.js
@@ -0,0 +1,11 @@
+var expect = require('chai').expect;
+var TB;
+
+describe('TB', function () {
+  before(function () {
+    TB = require('../index');
+  });
+
+  it('works', function () {
+  });
+});
