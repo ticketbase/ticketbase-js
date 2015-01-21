471074423a2d6dff1a0ef4be226a0a85f0739069
Rico Sta. Cruz <hi@ricostacruz.com>
2015-01-21 17:21:55 +0800

    Add uglify and promise-polyfill

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
+/*
+ * Ticketbase client
+ * @license MIT
+ */
+
 var TB = {};
 
+/*
+ * Promise polyfill
+ */
+
+var Promise =
+TB.Promise = this.Promise || require('promise-polyfill');
+
+/*
+ * scope
+ */
+
+var Scope =
 TB.Scope = function Scope() {};
+
+/*
+ * event
+ */
 
 TB.event = function () {
   return new Scope();
 };
+
+/*
+ * export
+ */
 
 module.exports = TB;
