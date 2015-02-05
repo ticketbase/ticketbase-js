06aa889c28e0b3373d048f184058f415726195f4
Rico Sta. Cruz <hi@ricostacruz.com>
2015-02-05 11:51:31 +0800

    Give up on unknown widget types

---
+++ test/unknown_widget_test.js
@@ -0,0 +1,17 @@
+require('./setup');
+
+describe('TB (unknown widget):', function () {
+  var widget, $w;
+
+  it("throws an error", function () {
+    expect(function () {
+      $w = $("<div id='w' data-tb='aoeu'>");
+      $('body').append($w);
+      widget = TB.widget($w[0]);
+    }).to.throw(/unknown widget type 'aoeu'/i);
+  });
+
+  afterEach(function (){
+    $w.remove();
+  });
+});
