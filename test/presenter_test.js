e6cb3b258138ecfa83267aec8a3f22dd3a8cc82e
Rico Sta. Cruz <hi@ricostacruz.com>
2015-03-04 19:25:27 +0800

    Test presenter is_closed and is_live

--- test/presenter_test.js
+++ test/presenter_test.js
@@ -9,15 +9,15 @@ var presentEvent = pquire('../lib/presenters/event', {
 });
 
 var extend = require('deep-extend');
-var event = require('./fixtures/event_ok.json');
+var original = require('./fixtures/event_ok.json');
 
 describe('presenter', function () {
-  var data;
+  var data, event;
 
   describe('ticket', function () {
     beforeEach(function () {
-      data = presentEvent(extend({}, event, {
-      }), 'ticket');
+      event = extend({}, original);
+      data = presentEvent(event, 'ticket');
     });
 
     describe('form hidden', function () {
@@ -28,6 +28,38 @@ describe('presenter', function () {
       it('has item IDs', function () {
         expect(data.form_hidden).include("order[order_items_attributes][0][item_id]");
       });
+    });
+  });
+
+  describe('ticket (closed)', function () {
+    beforeEach(function () {
+      event = extend({}, original);
+      event.status = 'live';
+      data = presentEvent(event, 'ticket');
+    });
+
+    it('sets is_closed to false', function () {
+      expect(data.is_closed).be.false;
+    });
+
+    it('sets is_live to true', function () {
+      expect(data.is_live).be.true;
+    });
+  });
+
+  describe('ticket (open)', function () {
+    beforeEach(function () {
+      event = extend({}, original);
+      event.status = 'closed';
+      data = presentEvent(event, 'ticket');
+    });
+
+    it('sets is_live to false', function () {
+      expect(data.is_live).be.false;
+    });
+
+    it('sets is_closed to true', function () {
+      expect(data.is_closed).be.true;
     });
   });
 
